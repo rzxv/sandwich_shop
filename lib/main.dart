9625c4c2d7fba915359dc184ea0149aa12a898ae
omarrashwani2004 <omarrashwani2004@gmail.com>
2025-10-17 11:48:06 +0000

    Define OrderItemDisplay custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,18 @@ Widget build(BuildContext context) {
 }
 }
 
+class OrderItemDisplay extends StatelessWidget {
+  final String itemType;
+  final int quantity;
+
+  const OrderItemDisplay(this.quantity, this.itemType, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('This is a placeholder for OrderItemDisplay');
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
