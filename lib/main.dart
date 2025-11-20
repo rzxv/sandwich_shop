07de91fb2239d2cc61a71676531a9132f6ed7fa5
omarrashwani2004 <omarrashwani2004@gmail.com>
2025-11-20 14:44:39 +0000

    Worksheet 3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -4,98 +4,8 @@ void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Sandwich Shop App',
-      // Use OrderScreen as home, setting maxQuantity to 5
-      home: OrderScreen(maxQuantity: 5),
-    );
-  }
-}
-
-class OrderScreen extends StatefulWidget {
-  // maxQuantity is immutable and defines the limit
-  final int maxQuantity;
-
-  const OrderScreen({super.key, this.maxQuantity = 10});
-
-  @override
-  State<OrderScreen> createState() {
-    return _OrderScreenState();
-  }
-}
-
-class _OrderScreenState extends State<OrderScreen> {
-  int _quantity = 0;
-
-  void _increaseQuantity() {
-    // Check against the immutable maxQuantity accessed via 'widget'
-    if (_quantity < widget.maxQuantity) {
-      // Must call setState() to trigger a UI rebuild
-      setState(() => _quantity++);
-    }
-  }
-
-  void _decreaseQuantity() {
-    if (_quantity > 0) {
-      // Must call setState() to trigger a UI rebuild
-      setState(() => _quantity--);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final VoidCallback? increaseCallback = _quantity < widget.maxQuantity
-        ? _increaseQuantity
-        : null;
-
-    final VoidCallback? decreaseCallback = _quantity > 0
-        ? _decreaseQuantity
-        : null;
-
-    return Scaffold(
-      appBar: AppBar(title: const Text('Sandwich Counter')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // We use the mutable state variable _quantity
-            OrderItemDisplay(_quantity, 'Footlong'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Expanded(
-                  child: StyledButton(
-                    onPressed: increaseCallback, // Use conditional callback
-                    text: 'Add',
-                    backgroundColor: Colors.green,
-                    foregroundColor: Colors.white,
-                  ),
-                ),
-                const SizedBox(width: 16),
-                Expanded(
-                  child: StyledButton(
-                    onPressed: decreaseCallback, // Use conditional callback
-                    text: 'Remove',
-                    backgroundColor: Colors.red,
-                    foregroundColor: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
 class StyledButton extends StatelessWidget {
-  final VoidCallback? onPressed; // The function to call when pressed
+  final VoidCallback? onPressed;
   final String text;
   final Color backgroundColor;
   final Color foregroundColor;
@@ -117,6 +27,127 @@ class StyledButton extends StatelessWidget {
         foregroundColor: foregroundColor,
       ),
       child: Text(text),
+    );
+  }
+}
+
+class OrderScreen extends StatefulWidget {
+  final int maxQuantity;
+
+  const OrderScreen({super.key, this.maxQuantity = 10});
+
+  @override
+  State<OrderScreen> createState() {
+    return _OrderScreenState();
+  }
+}
+
+class _OrderScreenState extends State<OrderScreen> {
+  int _quantity = 0;
+  String _itemType = 'Footlong';
+
+  void _increaseQuantity() {
+    if (_quantity < widget.maxQuantity) {
+      setState(() => _quantity++);
+    }
+  }
+
+  void _decreaseQuantity() {
+    if (_quantity > 0) {
+      setState(() => _quantity--);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final VoidCallback? increaseCallback = _quantity < widget.maxQuantity
+        ? _increaseQuantity
+        : null;
+
+    final VoidCallback? decreaseCallback = _quantity > 0
+        ? _decreaseQuantity
+        : null;
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Sandwich Counter')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() => _itemType = 'Six-inch');
+                    },
+                    child: const Text('Six-inch'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: _itemType == 'Six-inch'
+                          ? Colors.blue
+                          : Colors.grey,
+                      foregroundColor: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() => _itemType = 'Footlong');
+                    },
+                    child: const Text('Footlong'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: _itemType == 'Footlong'
+                          ? Colors.blue
+                          : Colors.grey,
+                      foregroundColor: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            // Use the mutable state variable _itemType
+            OrderItemDisplay(_quantity, _itemType),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  child: StyledButton(
+                    onPressed: increaseCallback,
+                    text: 'Add',
+                    backgroundColor: Colors.green,
+                    foregroundColor: Colors.white,
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: StyledButton(
+                    onPressed: decreaseCallback,
+                    text: 'Remove',
+                    backgroundColor: Colors.red,
+                    foregroundColor: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Sandwich Shop App',
+      home: OrderScreen(maxQuantity: 5),
     );
   }
 }
